175b3cb85fd5278cf1414001b16b355c5954cb7c
Sarah Flynn <sarah.flynn@nike.com>
2020-07-16 10:57:30 -0700

    public folder cleanup, company logo

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -2,12 +2,17 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+
+import logo from "../../assets/logo.svg";
 
 const useStyles = makeStyles((theme) => ({
   toolbarMargin: {
     ...theme.mixins.toolbar,
+    marginBottom: "2em",
+  },
+  logo: {
+    height: "7em",
   },
 }));
 
@@ -31,10 +36,8 @@ export default function Header(props) {
     <>
       <ElevationScroll>
         <AppBar position="fixed">
-          <Toolbar>
-            <Typography variant="h3" className={classes.title}>
-              Arc Development
-            </Typography>
+          <Toolbar disableGutters>
+            <img src={logo} alt="Arc Development company logo" />
           </Toolbar>
         </AppBar>
       </ElevationScroll>
