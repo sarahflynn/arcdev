350c6498dbb88fcef4c504240482de05a230580c
Sarah Flynn <sarah.flynn@nike.com>
2020-07-28 11:54:25 -0700

    landing page clean up

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Lottie from "react-lottie";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
 import {
   Button,
   Card,
@@ -17,19 +17,22 @@ import animationData from "../animations/landinganimation/data";
 import ButtonArrow from "./ui/ButtonArrow";
 import customSoftwareIcon from "../assets/Custom Software Icon.svg";
 import mobileAppsIcon from "../assets/mobileIcon.svg";
-import websiteIcon from "../assets/websiteIcon.svg";
+import websitesIcon from "../assets/websiteIcon.svg";
 import revolutionBackground from "../assets/repeatingBackground.svg";
 import infoBackground from "../assets/infoBackground.svg";
 
 const useStyles = makeStyles((theme) => ({
   animation: {
     maxWidth: "50em",
-    maxHeight: "21em",
+    minWidth: "21em",
     marginTop: "2em",
     marginLeft: "10%",
     [theme.breakpoints.down("sm")]: {
       maxWidth: "30em",
     },
+  },
+  buttonContainer: {
+    marginTop: "1em",
   },
   estimateButton: {
     ...theme.typography.estimate,
@@ -42,8 +45,26 @@ const useStyles = makeStyles((theme) => ({
       backgroundColor: theme.palette.secondary.light,
     },
   },
-  buttonContainer: {
-    marginTop: "1em",
+  heroTextContainer: {
+    minWidth: "21.5em",
+    marginLeft: "1em",
+    [theme.breakpoints.down("xs")]: {
+      marginLeft: 0,
+    },
+  },
+  icon: {
+    marginLeft: "2em",
+    [theme.breakpoints.down("xs")]: {
+      marginLeft: 0,
+    },
+  },
+  infoBackground: {
+    backgroundImage: `url(${infoBackground})`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    height: "100%",
+    width: "100%",
   },
   learnButton: {
     ...theme.typography.learnButton,
@@ -53,7 +74,6 @@ const useStyles = makeStyles((theme) => ({
     [theme.breakpoints.down("sm")]: {
       marginBottom: "2em",
     },
-    marginTop: 5,
   },
   learnButtonHero: {
     ...theme.typography.learnButton,
@@ -69,38 +89,6 @@ const useStyles = makeStyles((theme) => ({
     [theme.breakpoints.down("xs")]: {
       marginTop: "2em",
     },
-  },
-  heroTextContainer: {
-    minWidth: "21.5em",
-    marginLeft: "1em",
-    [theme.breakpoints.down("xs")]: {
-      marginLeft: 0,
-    },
-  },
-  specialText: {
-    fontFamily: "Pacifico",
-    color: theme.palette.common.orange,
-  },
-  subtitle: {
-    marginBottom: "1em",
-  },
-  icon: {
-    marginLeft: "2em",
-    [theme.breakpoints.down("xs")]: {
-      marginLeft: 0,
-    },
-  },
-  serviceContainer: {
-    marginTop: "12em",
-    [theme.breakpoints.down("sm")]: {
-      padding: 25,
-    },
-  },
-  service: {
-    marginBottom: "6em",
-  },
-  [theme.breakpoints.down("xs")]: {
-    marginTop: "4em",
   },
   revolutionBackground: {
     backgroundImage: `url(${revolutionBackground})`,
@@ -124,13 +112,18 @@ const useStyles = makeStyles((theme) => ({
       width: "100%",
     },
   },
-  infoBackground: {
-    backgroundImage: `url(${infoBackground})`,
-    backgroundPosition: "center",
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-    height: "100%",
-    width: "100%",
+  serviceContainer: {
+    marginTop: "12em",
+    [theme.breakpoints.down("sm")]: {
+      padding: 25,
+    },
+  },
+  specialText: {
+    fontFamily: "Pacifico",
+    color: theme.palette.common.orange,
+  },
+  subtitle: {
+    marginBottom: "1em",
   },
 }));
 
@@ -153,12 +146,13 @@ export default function LandingPage(props) {
   return (
     <Grid container direction="column" className={classes.mainContainer}>
       <Grid item>
-        {/*-----Hero Block------*/}
+        {/*-----Hero Block-----*/}
         <Grid container justify="flex-end" alignItems="center" direction="row">
-          <Grid item sm className={classes.heroTextContainer}>
-            <Typography align="center" variant="h2">
+          <Grid sm item className={classes.heroTextContainer}>
+            <Typography variant="h2" align="center">
               Bringing West Coast Technology
-              <br /> to the Midwest
+              <br />
+              to the Midwest
             </Typography>
             <Grid
               container
@@ -194,18 +188,19 @@ export default function LandingPage(props) {
               </Grid>
             </Grid>
           </Grid>
-          <Grid item sm className={classes.animation}>
+          <Grid sm item className={classes.animation}>
             <Lottie options={defaultOptions} height={"100%"} width={"100%"} />
           </Grid>
         </Grid>
       </Grid>
-      {/*------Services Block------*/}
-      <Grid item className={classes.serviceContainer}>
-        {/*-----Custom Software Block------*/}
+      <Grid item>
+        {" "}
+        {/*-----Custom Software Block-----*/}
         <Grid
           container
+          direction="row"
           justify={matchesSM ? "center" : undefined}
-          className={classes.service}
+          className={classes.serviceContainer}
         >
           <Grid
             item
@@ -248,16 +243,18 @@ export default function LandingPage(props) {
             />
           </Grid>
         </Grid>
-        {/*-----iOS/Android Block------*/}
+      </Grid>
+      <Grid item>
+        {/*-----iOS/Android Block-----*/}
         <Grid
           container
+          direction="row"
           justify={matchesSM ? "center" : "flex-end"}
-          className={classes.service}
+          className={classes.serviceContainer}
         >
           <Grid
             item
             style={{
-              marginLeft: matchesSM ? 0 : "5em",
               textAlign: matchesSM ? "center" : undefined,
             }}
           >
@@ -266,7 +263,7 @@ export default function LandingPage(props) {
               Extend Functionality. Extend Access. Increase Engagement.
             </Typography>
             <Typography variant="subtitle1">
-              Integrate your web expereince or create a standalone app
+              Integrate your web experience or create a standalone app
               {matchesSM ? " " : <br />}with either mobile platform.
             </Typography>
             <Button
@@ -290,16 +287,19 @@ export default function LandingPage(props) {
           <Grid item style={{ marginRight: matchesSM ? 0 : "5em" }}>
             <img
               className={classes.icon}
-              alt="mobile device icon"
+              alt="mobile phone icon"
               src={mobileAppsIcon}
             />
           </Grid>
         </Grid>
-        {/*-----Websites Block------*/}
+      </Grid>
+      <Grid item>
+        {/*-----Websites Block-----*/}
         <Grid
           container
+          direction="row"
           justify={matchesSM ? "center" : undefined}
-          className={classes.service}
+          className={classes.serviceContainer}
         >
           <Grid
             item
@@ -337,18 +337,18 @@ export default function LandingPage(props) {
             <img
               className={classes.icon}
               alt="website icon"
-              src={websiteIcon}
+              src={websitesIcon}
             />
           </Grid>
         </Grid>
       </Grid>
-      {/*-----Revolution Block------*/}
       <Grid item>
+        {/*-----The Revolution Block-----*/}
         <Grid
           container
+          style={{ height: "100em", marginTop: "12em" }}
           alignItems="center"
           justify="center"
-          style={{ height: "100vh", marginTop: "6em" }}
         >
           <Card className={classes.revolutionCard}>
             <CardContent>
@@ -365,7 +365,7 @@ export default function LandingPage(props) {
                 <Grid item>
                   <Typography variant="subtitle1">
                     Visionary insights coupled with cutting-edge technology is a
-                    recipe for revolution
+                    recipe for revolution.
                   </Typography>
                   <Button
                     onClick={() => setValue(2)}
@@ -388,12 +388,13 @@ export default function LandingPage(props) {
           <div className={classes.revolutionBackground} />
         </Grid>
       </Grid>
-      {/*-----About Block------*/}
       <Grid item>
+        {/*-----Information Block-----*/}
         <Grid
           container
-          style={{ height: "100vh" }}
+          style={{ height: "80em" }}
           alignItems="center"
+          direction="row"
           className={classes.infoBackground}
         >
           <Grid
@@ -411,13 +412,13 @@ export default function LandingPage(props) {
             >
               <Grid
                 container
-                direction="column"
                 style={{ marginBottom: matchesXS ? "10em" : 0 }}
+                direction="column"
               >
-                <Typography style={{ color: "white" }} variant="h2">
+                <Typography variant="h2" style={{ color: "white" }}>
                   About Us
                 </Typography>
-                <Typography variant="subtitle2">Let's Get Personal</Typography>
+                <Typography variant="subtitle2">Let's get personal.</Typography>
                 <Grid item>
                   <Button
                     onClick={() => setValue(3)}
@@ -442,13 +443,15 @@ export default function LandingPage(props) {
               }}
             >
               <Grid container direction="column">
-                <Typography style={{ color: "white" }} variant="h2">
+                <Typography variant="h2" style={{ color: "white" }}>
+                  Contact Us
+                </Typography>
+                <Typography variant="subtitle2">
                   Say hello!{" "}
                   <span role="img" aria-label="waving hand">
-                    🤗
+                    👋🏻
                   </span>
                 </Typography>
-                <Typography variant="subtitle2">Let's Get Personal</Typography>
                 <Grid item>
                   <Button
                     onClick={() => setValue(4)}
@@ -467,8 +470,8 @@ export default function LandingPage(props) {
           </Grid>
         </Grid>
       </Grid>
-      {/*-----Call to Action Block------*/}
       <Grid item>
+        {/*-----Call To Action Block-----*/}
         <CallToAction setValue={setValue} />
       </Grid>
     </Grid>
